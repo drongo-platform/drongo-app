356ba9309a2ebdaf2dd2cbbd71333b0009ef6885
Sattvik Chakravarthy <sattvik@Sattviks-MacBook-Pro.local>
2018-06-06 23:39:05 +0530

    Adding run_dev.

--- drongo_app/run.py
+++ drongo_app/run.py
@@ -8,10 +8,23 @@ import sys
 NEST = shutil.which('drongo-nest')
 
 
-def run():
+def run_dev():
     os.environ['DRONGO_SETTINGS_FILE'] = os.path.realpath(sys.argv[1])
     app = subprocess.Popen(
         ['python', '-m', 'nest.cmd', 'drongo_app.app:app', '--auto-reload'])
+
+    try:
+        app.wait()
+    except KeyboardInterrupt:
+        print('Terminating...')
+        app.terminate()
+        app.wait()
+
+
+def run():
+    os.environ['DRONGO_SETTINGS_FILE'] = os.path.realpath(sys.argv[1])
+    app = subprocess.Popen(
+        ['python', '-m', 'nest.cmd', 'drongo_app.app:app'])
 
     try:
         app.wait()
