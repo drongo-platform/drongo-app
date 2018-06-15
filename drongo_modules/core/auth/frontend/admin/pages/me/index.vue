74cd08f8ce1adb849370f5326f60e982b9e43651
Sattvik Chakravarthy <sattvik@Sattviks-MacBook-Pro.local>
2018-06-15 12:39:17 +0530

    Making change password responsive.

--- drongo_modules/core/auth/frontend/admin/pages/me/index.vue
+++ drongo_modules/core/auth/frontend/admin/pages/me/index.vue
@@ -5,7 +5,7 @@
       Auth / <small>Me</small>
     </h2>
     <div class="grid">
-      <div class="grid__col grid__col--04">
+      <div class="grid__col grid__col--d-04 grid__col--t-06">
         <div class="panel">
           <div class="panel__head">Change Password</div>
           <div class="panel__body">
